99ce7e8d89ed66720effd3ce42515768e1c7bf5c
williamslay_linux <williamslay.lsy@gmail.com>
2023-05-04 15:13:02 +0800

    easy test

---
+++ testing/test.sh
@@ -0,0 +1,15 @@
+export SUBJECT=/home/will/software_test/NJUPT_Work/NJUPT-Bachelor/testing/test
+export TMP_DIR=/home/will/software_test/NJUPT_Work/NJUPT-Bachelor/testing/temp
+
+pushd $SUBJECT
+make clean
+make
+popd
+
+cat $TMP_DIR/BBnames.txt | grep -v "^$"| rev | cut -d: -f2- | rev | sort | uniq > $TMP_DIR/BBnames2.txt && mv $TMP_DIR/BBnames2.txt $TMP_DIR/BBnames.txt
+cat $TMP_DIR/BBcalls.txt | grep -Ev "^[^,]*$|^([^,]*,){2,}[^,]*$"| sort  > $TMP_DIR/BBcalls2.txt && mv $TMP_DIR/BBcalls2.txt $TMP_DIR/BBcalls.txt
+
+for file in $TMP_DIR/dot-files/*.dot; do
+    filename=$(basename "$file")
+    dot -Tpng "$file" -o "$TMP_DIR/dot-files//${filename%.*}.png"
+done       
